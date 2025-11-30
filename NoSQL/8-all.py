ee09ff1845b3a6044aef8a6cd89d7148ca8ec1d1
kanan78 <kmusayev27@gmail.com>
2025-11-30 17:20:10 +0000

    commit

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""This module defines the function list_all"""
+
 def list_all(mongo_collection):
     """Lists all documents in a MongoDB collection."""
     docs = list(mongo_collection.find())
