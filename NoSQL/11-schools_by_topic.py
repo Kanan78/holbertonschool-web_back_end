17d065d835137564e48b282d48731bff749c5898
kanan78 <kmusayev27@gmail.com>
2025-11-30 17:34:48 +0000

    commit

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""This module defines the function schools_by_topic"""
+
+def schools_by_topic(mongo_collection, topic):
+    """This function returns the list of school having a specific topic"""
+    return list(mongo_collection.find({"topics": topic}))
