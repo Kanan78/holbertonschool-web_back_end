9e722ae617c457a7b1619854c8d83b286ab722d3
kanan78 <kmusayev27@gmail.com>
2025-11-30 17:28:48 +0000

    commit

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -3,5 +3,5 @@
 
 def insert_school(mongo_collection, **kwargs):
     """This function inserts a new document in a collection based on kwargs"""
-    result = mongo_collection.insert._one(kwargs)
+    result = mongo_collection.insert_one(kwargs)
     return result.inserted_id
